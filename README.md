0772ea89505f5e93209cf9df9939b4558f4cf216
AlfonsoLRz <allopezr@ujaen.es>
2022-08-04 19:26:42 +0200

    Change script name and prepare README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# TIFF2JPEG_DJIZenmuseXT2
- Image converter from TIFF to JPEG for files generated using the DJI Zenmuse XT2 device.
+# **TIFF2JPEG (DJI ZenmuseXT2)**
+Image converter from TIFF to JPEG for files captured from a **DJI Zenmuse XT2** device. Temperature values are stored in `.csv` files, whereas `.jpg` images are saved along with the `.tiff` metadata, so single viewpoints can be further processed through their metadata.
+
+![Thermal Composition](Assets/Teaser.png)
+
+### Methodology
+
+From an input folder, `.tiff` images are read and converted into `.jpg`, which are subsequently saved in another folder. Temperature from each pixel is also stored in `.csv` files. Then, exiftool is launched to transfer metadata from `.tiff` images to the previously generated `.jpg` files.
+
+### Usage
+
+The python script only requires input and output folders, as well as a boolean value indicating if device was configured in `high_gain` mode. By default it is false.
+
+> High gain mode changes the multiplying factor in the following formula
+**(img * factor - 273.15)** to transform from Kelvin to Celsius. High gain uses a factor of 0.04, whereas the alternative mode uses 0.4.
+
+`tiff2jpeg.py --input folder --output folder --high_gain`
+
+The script looks both for `.tiff` and `.tif` files in the input folder, so there is no need in passing the correct extension.
---
+++ tiff2jpeg.py
@@ -0,0 +1,61 @@
+import argparse
+import csv
+import cv2
+import glob
+import numpy as np
+import os
+import subprocess
+
+csv_extension = '.csv'
+jpeg_extension = '.jpg'
+script_folder = 'Scripts/'
+exiftool_exe = os.path.abspath(script_folder + 'exiftool.exe')
+temp_folder = 'temp/'
+tif_extensions = ['.tif', '.tiff']
+
+
+def export_temp_to_csv(filename, img):
+    with open(filename, 'w', newline='') as fh:
+        writer = csv.writer(fh, delimiter=',')
+        writer.writerow(['x', 'y', 'temp (c)'])
+        row_values = [[e[0][0], e[0][1], e[1]] for e in np.ndenumerate(img)]
+        writer.writerows(row_values)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("TIFF2JPEG")
+    parser.add_argument("--input", help="Input folder", type=str, default='In/', required=True)
+    parser.add_argument("--output", help="Output folder", type=str, default='Results/', required=True)
+    parser.add_argument('--high_gain', action=argparse.BooleanOptionalAction, default=False)
+    args = parser.parse_args()
+
+    if os.path.isdir(args.input):
+        if not os.path.isdir(args.output):
+            os.mkdir(args.output)
+
+        if not os.path.isdir(temp_folder):
+            os.mkdir(temp_folder)
+
+        for tif_extension in tif_extensions:
+            for path in glob.glob(args.input + '*' + tif_extension):
+                basename = os.path.basename(path)
+                basename = basename[0:len(basename) - len(tif_extension)]
+
+                tiff_img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+                factor = .04 if args.high_gain else .4
+                tiff_img = (tiff_img * factor - 273.15)             # From kelvin to degrees
+                max, min = np.max(tiff_img), np.min(tiff_img)
+                tiff_img = (tiff_img - min) / (max - min) * 255
+
+                # JPEG + CSV
+                cv2.imwrite(args.output + basename + jpeg_extension, tiff_img)
+                export_temp_to_csv(args.output + basename + csv_extension, tiff_img)
+
+                # Transfer metadata from TIFF to new JPEG
+                command_copy = exiftool_exe + ' -TagsFromFile ' + args.input + basename + tif_extension + \
+                                              ' -all:all>all:all ' + args.output + basename + jpeg_extension
+                p = subprocess.Popen(command_copy, stdout=subprocess.PIPE, shell=True, cwd='.')
+                if p.stdout.read() != b'':
+                    print(p.stdout.read())
+
+                print('Done processing of ' + path + '...')
